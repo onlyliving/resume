552d3691025b41ee02ae1adf6632855d344719c0
soheekim <onlyliving88@gmail.com>
2020-06-19 16:07:42 +0900

    [1] 이력서 내용 다듬기 - 텍스트에서 잘못된 어휘, 어색한 어휘 또는 뭉뚱그린 표현 찾아서 내용 수정. - 프로필 내용으로는 내가 어떤 색깔의 개발자인지, 어떤 경험을 했는지를 어필할 것.

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
                 {
                     company: '(주)휴마트컴퍼니',
                     position: '개발팀 - 프론트엔드 개발자',
-                    period: '2018.6.5 ~ 2019.12',
+                    period: '2018.6 ~ 2019.12',
                     link : 'https://trost.co.kr/',
                     skill : ['HTML5', 'CSS3', 'JavaScript', 'Webpack', 'Zeplin', 'GitLab', 'Trello', 'SourceTree', 'Postman', 'InteliJ', 'Visual Studio Code'],
                     workDetail : [
@@ -155,13 +155,13 @@ class App extends Component {
                         <article id="profileEl" className="content-box content-box--profile is-5-lines-only">
                             <h2 className="content-box__head"><span>Profile</span></h2>
                             <div className="content-box__profile-txt">
-                                웹 프론트엔드 개발자 김소희입니다.
-                                <br/>개발자로 일을 시작하기 전부터 웹사이트 디자인에 관심이 많았습니다. 기획과 디자인 의도대로 구현하는 것이 재미있고, 보람됩니다. 그리고 기획단에서 생각하지 못한 디테일을 체크하고, 같이 의견을 맞대어 더 나은 결과물로 발전될 때 더욱 보람을 느낍니다.
-                                <br/><br/>저는 스타트업에서 웹 프론트 개발 및 유지보수를 하였습니다. 바닐라 자바스크립트를 썼고, 웹팩으로 최적화하였습니다.
-                                이전 회사에서 JS 라이브러리나 프레임워크를 다루지 않았던 것은 ‘나중에 필요하면 도입’하는 것으로 개발팀 내에서 협의가 이뤄진 사항이었습니다. 그 이유는 제가 자바스크립트를 더 잘 다뤘으면 하는 바람이었을 것입니다.
-                                <br/><br/>스타트업에 들어오면서 기존에 다뤘던 제이쿼리는 모두 자바스크립트로 교체하였습니다. 그리고 처음에는 무조건 동작이 되게끔 하는 것이 목표였지만, 나중에는 실행되는 것을 넘어서 최적화하는 작업에 집중할 수 있게 되었습니다. 그렇게 개발 단계를 거치면서 잘 짜인 코드의 중요성과 필요성을 몸소 깨닫게 되었습니다.
-                                <br/><br/>그래서 저는 개발을 ‘잘’ 하는 개발자가 되고 싶습니다.
-                                <br/>지금은 구직활동을 하면서 <a id="sideProjectLinkText" href="#sideProjectHead" className="point-link">사이드 프로젝트</a>를 진행해보았고, 리엑트의 필요성을 알기 위해서 리엑트 공부를 하고 있습니다.
+                                <span className="txt-sub-title">어쩌다 개발자?</span><br/>
+                                let 김소희 = 조경설계 + 웹디자이너 + 웹퍼블리싱 + 웹 프론트엔드 개발;<br/><br/>
+                                저는 제가 자신 있는 것 하나(item)를 교집합 삼아 여기까지 왔습니다.<br/>
+                                조경설계에서 웹퍼블리셔로 직군을 변경할 때는 '디자인'이 자신 있어서 할 수 있었고, 웹퍼블리셔에서 웹 프론트엔드 개발로 갈 때에는 '웹 퍼블리싱'을 기반으로 발전할 수 있게 되었습니다.
+                                <br/><br/>
+                                <span className="txt-sub-title">웹 프론트엔드 개발자로서의 만족도는?</span><br/>
+                                기획과 디자인 의도대로 구현하는 것이 재미있습니다. 그리고 기획단에서 생각하지 못한 디테일을 체크해서, 같이 의견을 맞대어 더 나은 결과물로 발전될 때 더욱 보람을 느낍니다. 노력하는 만큼, 코드와 성과물로 고스란히 보이기 때문에 더욱 잘 해내고 싶습니다.
                             </div>
                             <button type="button" id="profileMoreBtn" className="content-box__profile-more-btn">more</button>
                         </article>
@@ -172,7 +172,7 @@ class App extends Component {
                                 <li>
                                     <div className="experience-list__name"><span>개발팀 - 프론트엔드 개발자</span><a className="point-link" href="https://trost.co.kr/" target="_blank" rel="noopener noreferrer">(주)휴마트컴퍼니</a></div>
                                     <ul>
-                                        <li className="experience-list__date">2018.6.5 ~ 2019.12</li>
+                                        <li className="experience-list__date">2018.6 ~ 2019.12, 1년 7개월</li>
                                         <li className="experience-list__item-box experience-box">
                                             <ul className="experience-box__item-list">
                                                 <li>HTML5</li>
@@ -191,15 +191,15 @@ class App extends Component {
                                         <li className="experience-list__txt experience-box">
                                             <span className="experience-box__head">PC 웹페이지 서비스 개발</span>
                                             <ul>
-                                                <li>- 입사 당시 모바일 서비스만 있고, PC 웹사이트를 개발해야 하는 단계에 있었음</li>
-                                                <li>- 기획에 따라 웹페이지 개선 및 구축. (html 마크업 및 퍼블리싱, RESTful API 연결 등 기능 구현)</li>
-                                                <li>- 깃랩으로 이슈 관리 (기획, 마케팅 등과 협의 시에는 트렐로로 관리)</li>
+                                                <li>- 입사 후 개발 시작 단계 : PC 웹사이트에 서비스를 도입하는 초기 단계 (모바일앱 서비스만 존재)</li>
+                                                <li>- 기획에 따라 웹페이지 기능 개선 및 구축 (HTML 마크업 및 퍼블리싱, RESTful API 연결 등 기능 구현)</li>
+                                                <li>- 깃랩(GitLab)으로 이슈 관리 (기획, 마케팅 등과 협의 시에는 트렐로(Trello)로 관리)</li>
                                             </ul>
                                         </li>
                                         <li className="experience-list__txt experience-box">
                                             <span className="experience-box__head">앱 내의 웹페이지 서비스 개발</span>
                                             <ul>
-                                                <li>- 개발팀 협의로 앱 내에 웹페이지 개발로 필요한 부분은 웹 포지션으로 개발 (개발 변경이 잦은 서비스 페이지 등)</li>
+                                                <li>- 개발팀 협의로 앱 내에 웹페이지 개발이 필요한 부분은 웹 포지션으로 개발 (개발 변경이 잦은 서비스 페이지 등)</li>
                                             </ul>
                                         </li>
                                         <li className="experience-list__inner-box">데이터를 다루는 것을 알게 되었습니다. (서버 개발자와 API 요청과 응답에 대한 규칙 정하기, 언제 어떤 데이터를 불러와야 하는지 등) 웹앱에서 앱과의 통신하는 협업 과정도 기억에 크게 남습니다.</li>
@@ -208,7 +208,7 @@ class App extends Component {
                                 <li>
                                     <div className="experience-list__name"><span>개발팀 - 웹퍼블리셔</span><a className="point-link" href="https://roomnspace.co.kr/" target="_blank" rel="noopener noreferrer">(주)룸앤스페이스</a></div>
                                     <ul>
-                                        <li className="experience-list__date">2016.9 ~ 2018.5</li>
+                                        <li className="experience-list__date">2016.9 ~ 2018.5, 1년 9개월</li>
                                         <li className="experience-list__item-box experience-box">
                                             <ul className="experience-box__item-list">
                                                 <li>HTML5</li>
@@ -222,18 +222,17 @@ class App extends Component {
                                         <li className="experience-list__txt experience-box">
                                             <span className="experience-box__head">웹사이트와 모바일웹 구축을 위한 디자인 및 퍼블리싱 작업</span>
                                             <ul>
-                                                <li>- 입사 당시 서버 구축만 되어있는 상태에서 처음부터 같이 개발을 시작하였음</li>
+                                                <li>- 입사 후 개발 시작 단계 : 서버 세팅만 되어있는 상태에서 처음부터 개발을 시작하는 단계</li>
                                                 <li>- 웹 작업 비율(%) = 웹 디자인 작업 70% + 퍼블리싱 30%</li>
-                                                <li>- 웹 디자인과 퍼블리싱 작업을 병행하여, 비교적 디자인 작업을 많이 함</li>
                                             </ul>
                                         </li>
-                                        <li className="experience-list__inner-box">웹디자인(UI, UX)에 대한 감각을 키웠습니다. 서버 개발자와 협업하면서 작업관리 시스템의 필요성을 느꼈습니다. (겨우 두명이지만 같이 코드를 공유하면서 업데이트 되어야 할 코드가 지워지는 등 코드 관리 문제가 있었습니다.) 웹 퍼블리셔로 일하면서 개발에 대한 갈증이 더욱 생겼습니다.</li>
+                                        <li className="experience-list__inner-box">웹디자인(UI, UX)에 대한 감각을 키웠습니다. 서버 개발자와 협업하면서 작업관리 시스템의 필요성을 느꼈습니다. (겨우 두 명이지만 같이 코드를 공유하면서 업데이트되어야 할 코드가 지워지는 등 코드 관리 문제가 있었습니다.) 웹 퍼블리셔로 일하면서 개발에 대한 갈증이 더욱 생겼습니다.</li>
                                     </ul>
                                 </li>
                                 <li>
                                     <div className="experience-list__name"><span>설계팀 - 디자이너</span><a className="point-link" href="http://www.ehwawon.kr/2016_ehwawon.pdf" target="_blank" rel="noopener noreferrer">조경설계 이화원</a></div>
                                     <ul>
-                                        <li className="experience-list__date">2011.3 ~ 2015.10</li>
+                                        <li className="experience-list__date">2011.3 ~ 2015.10, 4년 8개월 </li>
                                         <li className="experience-list__item-box experience-box">
                                             <ul className="experience-box__item-list">
                                                 <li>AutoCAD</li>
@@ -243,7 +242,7 @@ class App extends Component {
                                             </ul>
                                         </li>
                                         <li className="experience-list__txt experience-box">계획설계/기본설계/실시설계</li>
-                                        <li className="experience-list__inner-box">세상을 보는 시야가 넓어졌습니다. 길을 걷다가도 바닥 포장재와 패턴, 배수 형태, 어떤 조경수를 심었는지, 옥외 계단 구조물 등 설계가 눈에 보이고, 잘 꾸며진 공간일수록 그 디테일이 더 눈에 들어옵니다.</li>
+                                        <li className="experience-list__inner-box">세상을 보는 시야가 넓어졌습니다. 길을 걷다가도 바닥 포장재와 패턴, 배수 형태, 어떤 조경수를 심었는지, 옥외 계단 구조물 등 설계가 머릿 속에 대략 그려집니다. 공간과 구조물, 시설물의 디테일을 보면 그 공간에 얼마나 노력이 들어갔는지 알 수 있습니다.</li>
                                     </ul>
                                 </li>
                             </ul>
@@ -317,9 +316,9 @@ class App extends Component {
                             <dl className="table-box">
                                 <dt className="table-box__q" value="q-1">업무상 강점은 무엇인가요?</dt>
                                 <dd className="table-box__a" value="a-1">
-                                    웹디자인 경험이 있어서 기획, 디자인팀과 업무 커뮤니케이션이 원만합니다. 필요하면 간단한 디자인은 제가 하기도 합니다.
-                                    <br/>일에 대한 책임감과 욕심이 있습니다.
-                                    <br/>일은 제 삶의 중요한 원동력이기 때문에 더 잘하려고 노력하고, 같이 일하는 사람들과 같이 성장하고 싶습니다.
+                                    웹디자인 경험이 있어서 기획, 디자인팀과 업무 커뮤니케이션이 원만합니다. 필요하면 간단한 디자인은 제가 할 수도 있습니다.
+                                    <br/>웹페이지 퍼블리싱 작업의 구현 속도가 빠릅니다.
+                                    <br/>일에 대한 책임감이 있습니다. 당연한 말이지만, 정해진 시간 내에 맡은 업무를 잘 수행하려고 하고, 갑작스런 버그나 이슈가 생기면 적극적으로 나섭니다.
                                 </dd>
                                 <dt className="table-box__q" value="q-3">웹 개발에서 버그 이슈가 생겼을 경우 어떻게 대처하나요?</dt>
                                 <dd className="table-box__a" value="a-3">
@@ -343,13 +342,12 @@ class App extends Component {
                                 </dd> */}
                                 <dt className="table-box__q" value="q-5">일에 대한 가치는 어떤가요?</dt>
                                 <dd className="table-box__a" value="a-5">
-                                    
-                                    일은 삶에서 많은 시간을 차지하기에 중요하고, 사회에서 가치를 실현할 수 있는 좋은 도구라고 생각합니다.
-                                    <br/>일에 몰입하는 것을 좋아하고, 일과 함께 저도 같이 잘 성장해나가고 싶습니다.
-                                    <br/>그래서 제가 좀 더 잘 할 수 있는, 재미있는, 좋은 서비스를 가진, 발전 가능성이 있는 일을 하고 싶습니다.
+                                    일은 삶에서 많은 비중을 차지하기 때문에, 일 하는 것이 곧 삶이라고 여깁니다. 그리고 일은 사회에서 '나'의 가치를 실현할 수 있는 좋은 도구라고 생각합니다.
+                                    <br/>개인 취향이지만, 몰입해서 일 하는 것을 좋아하여 음악도 잘 듣지 않습니다. 그리고 일을 잘 해나감으로써 저 스스로도 같이 잘 성장해나가고 싶습니다.
+                                    <br/>그래서 저는 제가 좀 더 잘 할 수 있는, 재미있는, 좋은 서비스를 가진, 발전 가능성이 있는 일을 하고 싶습니다.
                                 </dd>
-                                <dt className="table-box__q" value="q-6">원하는 회사 위치는 어디인가요?</dt>
-                                <dd className="table-box__a" value="a-6">1. 판교 2. 서울(강남)</dd>
+                                {/* <dt className="table-box__q" value="q-6">원하는 회사 위치는 어디인가요?</dt>
+                                <dd className="table-box__a" value="a-6">1. 판교 2. 서울(강남)</dd> */}
                                 <dt className="table-box__q" value="q-7">최종학력은 어떻게 되나요?</dt>
                                 <dd className="table-box__a" value="a-7">청주대학교 환경조경학과(4년제)를 졸업했습니다.</dd>
                                 
@@ -359,11 +357,11 @@ class App extends Component {
                                     이 일을 오래 하고 나중에는 '내'가 설계를 잘 이끌어갈 수 있을까, 지금도 잦은 야근에 몸이 너무 힘든데 앞으로도 버틸 수 있을까에 대한 의문이 들었습니다.
                                     제 위의 상사를 보더라도 크게 다르지 않았습니다. 아니 더욱 힘들어 보였습니다.
                                     <br/><br/>그래서 좀 더 저에게 맞는 직업을 다시 찾고자 하였고, 디자인을 계속 다뤄왔으니 디자인과 관련된 일을 찾고 싶었습니다.
-                                    <br/>웹 사이트는 친숙하고 매일 접합니다. 그래서 평소에도 웹사이트 디자인에 관심이 있었습니다. 웹사이트를 만드는 것이 재미있어 보여서 시작하게 되었습니다.
+                                    <br/>웹 사이트는 친숙하고 매일 접합니다. 그래서 평소에도 눈길을 끄는 웹사이트를 발견하면 '즐겨찾기'에 추가하곤 했습니다. 웹사이트를 만드는 것이 재미있어 보여서 시작하게 되었습니다.
                                 </dd>
                                 <dt className="table-box__q" value="q-10">퍼블리셔에서 프론트엔드 개발자가 된 계기는 무엇인가요?</dt>
                                 <dd className="table-box__a" value="a-10">
-                                    웹 퍼블리셔로 웹을 다루기 시작하다가 디자인 작업보다 프론트단 개발하는 것에 욕심이 생겼습니다. 웹 디자인, HTML 마크업, CSS까지는 완전히 저의 담당이었고, JS 동적인 부분은 데이터 연결 전까지 할 수 있는 작업을 모두 해서 넘기는 식이었습니다. 그러다가 자바스크립트 영역을 더 많이, 잘 다루고 싶어졌고, 개발 관련하여 여러 스터디를 다니다가 좋은 인연을 만나서 스타트업 개발팀에서 프론트엔드 개발자로 일을 하게 되었습니다
+                                    웹 퍼블리셔로 웹을 다루기 시작하다가 디자인 작업보다 프론트단 개발하는 것에 욕심이 생겼습니다. 그 당시에 웹 디자인, HTML 마크업, CSS까지는 완전히 저의 담당이었고, JS 동적인 부분은 데이터 연결 전까지 할 수 있는 작업을 모두 해서 넘기는 식이었습니다. 그러다가 자바스크립트 영역을 더 많이, 잘 다루고 싶어졌고, 개발 관련하여 여러 스터디(프론트 백엔드 스터디 등)를 다니다가 좋은 인연을 만나서 스타트업 개발팀에서 프론트엔드 개발자로 일을 하게 되었습니다
                                 </dd>
                                 {/* <dt className="table-box__q" value="q-2">희망연봉이 있나요?</dt>
                                 <dd className="table-box__a" value="a-2">
@@ -453,8 +451,11 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
     
-    document.getElementById('sideProjectLinkText').addEventListener('click', function() {
-        document.querySelectorAll('.side-project-list > li')[0].click();
-    });
+    if (document.getElementById('sideProjectLinkText')) {
+        document.getElementById('sideProjectLinkText').addEventListener('click', function() {
+            document.querySelectorAll('.side-project-list > li')[0].click();
+        });
+    }
+    
 });
 
