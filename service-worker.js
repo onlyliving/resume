d88f326f10838dcdfaee3bdb501510571288e488
soheekim <greensohee88@naver.com>
2020-12-08 22:35:13 +0900

    서비스워커 테스트

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v66';
+const CACHE_NAME = 'static-cache-v90';
 
 const URL_PATH_NAME = (location.hostname === 'localhost') ? '/' : '/resume/';
 // console.log(URL_PATH_NAME);
@@ -14,11 +14,11 @@ const FILES_TO_CACHE = [
 ]
 
 self.addEventListener('install', (event) => {
-    console.log('>>> [ServiceWorker] Install <<<');
+    console.log('%c >>> [ServiceWorker] Install <<<', 'color:green');
     // Precache static resources here.
     event.waitUntil(
       caches.open(CACHE_NAME).then((cache) => {
-        console.log('[ServiceWorker] Pre-caching offline page');
+        console.log('%c [ServiceWorker] Pre-caching offline page', 'color:green');
         return cache.addAll(FILES_TO_CACHE);
       })
     );
@@ -27,13 +27,13 @@ self.addEventListener('install', (event) => {
 });
   
 self.addEventListener('activate', (event) => {
-    console.log('>>> [ServiceWorker] Activate <<<');
+    console.log('%c >>> [ServiceWorker] Activate <<<', 'color:green');
     // Remove previous cached data from disk.
     event.waitUntil(
       caches.keys().then((keyList) => {
         return Promise.all(keyList.map((key) => {
           if (key !== CACHE_NAME) {
-            console.log('[ServiceWorker] Removing old cache -->', key);
+            console.log(`%c [ServiceWorker] Remove old cache --> key : ${key}`, 'color:green');
             return caches.delete(key);
           }
         }));
@@ -44,7 +44,7 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', event => {
-    console.log(`>>> [ServiceWorker] fetch <<<`);
+    console.log(`%c >>> [ServiceWorker] fetch <<<`, 'color:green');
     const req = event.request;
     const url = new URL(req.url);
 
@@ -65,7 +65,7 @@ self.addEventListener('fetch', event => {
 // 캐시 자체에 있는 파일과 요청을 일치시키는 함수
 // 요청은 고유한 키(key)처럼 동작한다. 이 함수는 캐시에 아무 것도 없을 경우 undefined를 반환하거나 캐시 요청 자체를 반환한다.
 async function cacheFirst(req){
-    console.log(`>>> [ServiceWorker] cacheFirst <<<`);
+    console.log(`%c >>> [ServiceWorker] cacheFirst <<<`, 'color:green');
     const cachedResponse = caches.match(req);
     return cachedResponse || fetch(req);
 }
@@ -73,7 +73,7 @@ async function cacheFirst(req){
 // 네트워크 첫 접근(network-first approach)을 생성하기 위해서는, 필요할 경우에 모든 네트워크 에셋이 위치한 곳에 동적 캐시를 만들어야 한다.
 // 패치가 진행되고 불가능한 경우 정적 캐시가 반환된다.
 async function newtorkFirst(req){
-    console.log(`>>> [ServiceWorker] newtorkFirst <<<`);
+    console.log(`%c >>> [ServiceWorker] newtorkFirst <<<`, 'color:green');
     const cache = await caches.open('dynamic-cache');
 
     try {
