a27925c76886cb6c616e0b8f556e4a966a7cab6e
soheekim <greensohee88@naver.com>
2022-01-27 11:30:33 +0900

    [1] 서비스워커 버전 업데이트

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // Update cache names any time any of the cached files change.
-const CACHE_NAME = "static-cache-v92";
+const CACHE_NAME = "static-cache-v93";
 
 const URL_PATH_NAME = location.hostname === "localhost" ? "/" : "/resume/";
 // console.log(URL_PATH_NAME);
